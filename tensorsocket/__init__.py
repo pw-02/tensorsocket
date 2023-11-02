2a094bbe43c7b8f395ead180450fbbcc846cde81
Neil <neni@itu.dk>
2023-11-02 12:06:45 +0100

    Bump version

--- tensorsocket/__init__.py
+++ tensorsocket/__init__.py
@@ -1,7 +1,7 @@
 """Data loading orchestrator for separating loading and training into different processes, sharing data one-to-many and connected via TCP."""
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
-from .producer import TensorProducer
 from .consumer import TensorConsumer
 from .payload import TensorPayload
+from .producer import TensorProducer
